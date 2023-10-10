f173696050a49c29b26feeac9698912fbbece38a
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:22:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_tags import st_tags
 
 st.title("test")
-st.multiselect( "Choose countries", ["1","2"], ["China", "United States of America"])
+st.multiselect( "Choose countries", ["China", "United States of America","test"], ["China", "United States of America"])
