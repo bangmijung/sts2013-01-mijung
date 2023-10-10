90997360e3e1972651b83f9518bb93c41513183e
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:23:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title("test")
-st.multiselect( "Choose countries", ["China", "United States of America","test"], ["China", "United States of America"])
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
