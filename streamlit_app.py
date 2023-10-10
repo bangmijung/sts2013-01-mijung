a216c809a60590743fd97e76def0001dc22f7106
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:19:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 from streamlit_tags import st_tags
 
 st.title("test")
-labels_from_st_tags = st_tags(value=["사회/경제", "안전", "교통"],maxtags=3,label="")
+st.multiselect( "Choose countries", ["1","2"], ["China", "United States of America"])
