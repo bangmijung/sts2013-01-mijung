8b84d2bd44694ba77e5efd60d4ca2991314b218b
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:12:22 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
-
+from streamlit_tags import st_tags
 st.title("test")
+st_tags(
+            value=["test","Test2"],
+            maxtags=3,
+            label=""
+        )
