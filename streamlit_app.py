72b7487a0aee015e61a1b1c371ea52ba4023f024
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:31:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from streamlit.hello.utils import show_code
-
+from streamlit_tags import st_tags
 st.title("test")
 options = st.multiselect(
     label = 'What are your favorite colors',
     options = ['Green', 'Yellow', 'Red', 'Blue'],
     default = ['Yellow', 'Red'])
+labels_from_st_tags = st_tags(
+            value=["사회/경제", "안전", "교통"],
+            maxtags=3,
+            label=""
+        )
