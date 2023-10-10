980514364b29efbca5922e9fd4a44706e6d963f3
bangmijung <66621920+bangmijung@users.noreply.github.com>
2023-10-10 23:26:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+import altair as alt
+import pandas as pd
+
+from streamlit.hello.utils import show_code
 st.title("test")
 options = st.multiselect(
     label = 'What are your favorite colors',
